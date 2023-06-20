a94dc89a87a1f3d69b3158314f5f32c18adbaa49
Ahmet Melih Çalmaz <114925956+acalmaz@users.noreply.github.com>
2023-06-20 15:57:13 +0300

    Update Z95625.CBL(ODEV1).cbl

--- Z95625.CBL(ODEV1).cbl
+++ Z95625.CBL(ODEV1).cbl
@@ -4,39 +4,39 @@
        AUTHOR.        Melih Çalmaz
       *--------------------
        ENVIRONMENT DIVISION.
-      *Aşağıdaki satır, programın girdi ve çıktıları bölümünün başlangıcını beli
+      *Aşağıdaki satır, programın girdi ve çıktıları bölümünün başlangıcını belirtir.
        INPUT-OUTPUT SECTION.
       *Aşağıdaki satır, programın dosya kontrol bölümünün başlangıcını belirtir.
        FILE-CONTROL.
-      *14. satırda, "PRINT-LINE" adlı bir dosya seçilir ve "PRTLINE" adıyla atam
-      *15. satırda, "ACCT-REC" adlı bir dosya seçilir ve "ACCTREC" adıyla atamas
+      *14. satırda, "PRINT-LINE" adlı bir dosya seçilir ve "PRTLINE" adıyla atar.
+      *15. satırda, "ACCT-REC" adlı bir dosya seçilir ve "ACCTREC" adıyla atar.
            SELECT PRINT-LINE ASSIGN TO PRTLINE.
            SELECT ACCT-REC   ASSIGN TO ACCTREC.
-      *SELECT yan tümcesi dahili bir dosya adı oluşturur
+      *SELECT yan tümcesi dahili bir dosya adı oluşturur.
       *ASSIGN yan tümcesi, harici bir veri kaynağı için bir ad oluşturur,
-      *z/OS tarafından kullanılan JCL DDNAME ile ilişkilidir
+      *z/OS tarafından kullanılan JCL DDNAME ile ilişkilidir.
       *Örneğin. ACCTREC, CBL0001J JCL dosyasında &SYSUID..DATA'ya bağlanmıştır.
-      *burada &SYSUID. z/OS kullanıcı kimliğiniz anlamına gelir
+      *burada &SYSUID. z/OS kullanıcı kimliğiniz anlamına gelir.
       *Örneğin. kullanıcı kimliğiniz Z95625 ise,
       *ACTREC için kullanılan veri seti Z95625.DATA'dır.
        DATA DIVISION.
       *-------------
        FILE SECTION.
-      *Alt satır, "PRINT-LINE" adlı dosyanın formatının "F" (fixed) olduğunu bel
+      *Alt satır, "PRINT-LINE" adlı dosyanın formatının "F" (fixed) olduğunu belirtir.
        FD  PRINT-LINE RECORDING MODE F.
-      *"PRINT-REC" adlı bir kayıt tanımlanır. Bu kayıt, "PRINT-LINE" dosyasının 
-      *32. satır, "ACCT-NO-O" adlı bir alan tanımlar. Bu alan, 8 karakter uzunlu
-      *33. satır, "ACCT-LIMIT-O" adlı bir alan tanımlar. Bu alan, dolar simgeler
+      *"PRINT-REC" adlı bir kayıt tanımlanır. Bu kayıt, "PRINT-LINE" dosyasının yapısını temsil eder.
+      *32. satır, "ACCT-NO-O" adlı bir alan tanımlar. Bu alan, 8 karakter uzunluğundadır.
+      *33. satır, "ACCT-LIMIT-O" adlı bir alan tanımlar.Bu alan dolar simgeleri ve ondalık basamaklar içerir.
       *ve "PRINT-REC" kaydının bir parçasıdır.
        01  PRINT-REC.
            05  ACCT-NO-O      PIC X(8).
            05  ACCT-LIMIT-O   PIC $$,$$$,$$9.99.
            05  ACCT-BALANCE-O PIC $$,$$$,$$9.99.
-      *36. Satır,"LAST-NAME-O" adlı bir alan tanımlar. Bu alan, 20 karakter uzun
+      *36. Satır,"LAST-NAME-O" adlı bir alan tanımlar. Bu alan, 20 karakter uzunluğundadır.
            05  LAST-NAME-O    PIC X(20).
            05  FIRST-NAME-O   PIC X(15).
            05  COMMENTS-O     PIC X(50).
-      *05 seviyesi 01 seviyesinden düşük olduğu için tüm değişkenler PRINT-REC'e
+      *05 seviyesi 01 seviyesinden düşük olduğu için tüm değişkenler PRINT-REC'e aittir.
        FD  ACCT-REC RECORDING MODE F.
        01  ACCT-FIELDS.
            05  ACCT-NO            PIC X(8).
@@ -53,7 +53,7 @@
            05  RESERVED           PIC X(7).
            05  COMMENTS           PIC X(50).
       *
-      *59.satır, "LASTREC" adlı bir alan tanımlar. Bu alan, "SPACE" değeriyle ba
+      *59.satır, "LASTREC" adlı bir alan tanımlar. Bu alan, "SPACE" değeriyle başlatılan bir karakter alanıdır.
        WORKING-STORAGE SECTION.
        01 FLAGS.
          05 LASTREC           PIC X VALUE SPACE.
@@ -95,19 +95,20 @@
            MOVE FIRST-NAME   TO  FIRST-NAME-O.
            MOVE COMMENTS     TO  COMMENTS-O.
            WRITE PRINT-REC.
-      *READ-RECORD.: Bu bölüm, bir kaydı okumak için ACCT-REC dosyasını okur.
+      
+       *READ-RECORD.: Bu bölüm, bir kaydı okumak için ACCT-REC dosyasını okur.
       *READ ACCT-REC: ACCT-REC dosyasından bir kaydı okur.
-      *AT END: Dosyanın sonuna gelindiğinde gerçekleşecek olan bir kontrol nokta
-      *MOVE 'Y' TO LASTREC: Dosyanın sonuna gelindiğinde, LASTREC değişkenine 'Y
+      *AT END: Dosyanın sonuna gelindiğinde gerçekleşecek olan bir kontrol noktasını belirtir.
+      *MOVE 'Y' TO LASTREC: Dosyanın sonuna gelindiğinde, LASTREC değişkenine 'Y' atanır. 
       *Bu, döngünün sonlanmasını sağlayacak bir kontrol mekanizmasıdır.
       *END-READ.: Dosya okuma işlemini sonlandırır.
-      *WRITE-RECORD.: Bu bölüm, bir kaydı PRINT-LINE dosyasına yazmak için kulla
-      *MOVE ACCT-NO TO ACCT-NO-O: ACCT-NO alanının değeri ACCT-NO-O alanına taşı
-      *MOVE ACCT-LIMIT TO ACCT-LIMIT-O: ACCT-LIMIT alanının değeri ACCT-LIMIT-O 
-      *MOVE ACCT-BALANCE TO ACCT-BALANCE-O: ACCT-BALANCE alanının değeri ACCT-BA
-      *MOVE LAST-NAME TO LAST-NAME-O: LAST-NAME alanının değeri LAST-NAME-O alan
-      *MOVE FIRST-NAME TO FIRST-NAME-O: FIRST-NAME alanının değeri FIRST-NAME-O 
-      *MOVE COMMENTS TO COMMENTS-O: COMMENTS alanının değeri COMMENTS-O alanına 
-      *WRITE PRINT-REC.: PRINT-REC yapısındaki kaydı PRINT-LINE dosyasına yazmak
-      *Bu bölümler, ACCT-REC dosyasından bir kaydı okuyarak ilgili alanları PRIN
+      *WRITE-RECORD.: Bu bölüm, bir kaydı PRINT-LINE dosyasına yazmak için kullanılır.
+      *MOVE ACCT-NO TO ACCT-NO-O: ACCT-NO alanının değeri ACCT-NO-O alanına taşınır.
+      *MOVE ACCT-LIMIT TO ACCT-LIMIT-O: ACCT-LIMIT alanının değeri ACCT-LIMIT-O alanına taşınır.
+      *MOVE ACCT-BALANCE TO ACCT-BALANCE-O: ACCT-BALANCE alanının değeri ACCT-BALANCE-O alanına taşınır.
+      *MOVE LAST-NAME TO LAST-NAME-O: LAST-NAME alanının değeri LAST-NAME-O alanına taşınır.
+      *MOVE FIRST-NAME TO FIRST-NAME-O: FIRST-NAME alanının değeri FIRST-NAME-O alanına taşınır.
+      *MOVE COMMENTS TO COMMENTS-O: COMMENTS alanının değeri COMMENTS-O alanına taşınır.
+      *WRITE PRINT-REC.: PRINT-REC yapısındaki kaydı PRINT-LINE dosyasına yazmak için kullanılır.
+      *Bu bölümler, ACCT-REC dosyasından bir kaydı okuyarak ilgili alanları PRINT-LINE dosyasına yazan bir döngüyü temsil eder. 
       *Döngü, dosyanın sonuna gelindiğinde sonlanır.
